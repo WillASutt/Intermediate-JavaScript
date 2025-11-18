3bdd4bb0c2474685d7157047cf7cf1f635bd8a42
William Andrew Sutton <williama1992@outlook.com>
2025-11-17 23:45:01 -0500

    create Space-Mission file

---
+++ Space-Mission/space-mission-control.js
@@ -0,0 +1,43 @@
+// Task 1: Declare Task Array and Interval ID
+const oneTimeTasks = [];
+const monitoringTaskId = [];
+
+// Task 2: Add One-Time Task Function
+
+function addOneTimeTask(func, delay) {
+
+}
+
+// Task 3: Run One-time Task Function
+
+function runOneTimeTask() {
+
+}
+
+// Task 4: Start Monitoring Function
+
+function startMonitoring() {
+
+}
+
+
+// Task 5: Stop Monitoring Function
+
+function stopMonitoring() {
+
+}
+
+// Task 6: Start Countdown Function
+
+function startCountdown(duration) {
+
+}
+
+// Task 7: Schedule Pre-launch Activities and Launch
+function scheduleMission() {
+
+}
+
+// Task 8:
+
+scheduleMission();
